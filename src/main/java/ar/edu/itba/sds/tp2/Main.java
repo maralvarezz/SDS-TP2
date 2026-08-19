c95c3eadc4c85467f0d9a4493994eefcc6c3d987
SantiSanchezMar <ssanchezmarostica@itba.edu.ar>
2026-08-19 16:17:53 -0300

    FEAT: create maven project
    Co-Authored-By: Maria Victoria Alvarez <146204185+maralvarezz@users.noreply.github.com>

---
+++ src/main/java/ar/edu/itba/sds/tp2/Main.java
@@ -0,0 +1,10 @@
+﻿package ar.edu.itba.sds.tp2;
+
+import ar.edu.itba.sds.model.Particle;
+
+public class Main {
+    public static void main(String[] args) {
+        Particle particle = new Particle(1, 0.0, 0.0, 0.5, 1.0);
+        System.out.println(particle);
+    }
+}
